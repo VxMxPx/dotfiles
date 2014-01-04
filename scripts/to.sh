2eb7b252603ac52fcbd6cf064dd13b6e37fc9917
Marko <m@avrelia.com>
2014-01-04 21:27:27 +0100

    Inproved window search.

--- scripts/to.sh
+++ scripts/to.sh
@@ -2,18 +2,17 @@
 
 # Find window by name and focus it, or go to desktop N
 
-if [[ $1 =~ ^-?[1-9]$ ]]; then
+if [[ $1 =~ ^[1-9]$ ]]; then
 	num=$(( $1 - 1 ));
 	if [ $1 -gt `xdotool get_num_desktops` ]; then
 		xdotool set_num_desktops $1;
 	fi
 	xdotool set_desktop $num;
 else
-    line=-${2:-1}
-    name=$(xdotool search --name $1 | tail $line)
+    line=${2:-1}
+    window_id=$(wmctrl -l | grep -i $1 | sed -n $line"p" | grep -oEi 0x[0-9a-f]+)
 
-    if [ "x$name" != "x" ]
-    then
-         xdotool windowactivate "$name";
+    if [[ $window_id != "" ]]; then
+        wmctrl -a $window_id -i
     fi
 fi
