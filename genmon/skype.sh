b9b613d4e92118c89066dc28c02a0a530392f421
Marko <m@gaj.st>
2015-04-23 14:10:18 +0200

    Skype, reported if closed (kind of) new icons.

--- genmon/skype.sh
+++ genmon/skype.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-messages=$(wmctrl -l | grep Skype™ | awk '{ print $4 }' | grep -i '\[[0-9]*\]' | cut -d ] -f 1 | cut -d [ -f 2)
+skype=$(wmctrl -l | grep Skype™);
 
-if [[ "x$messages" == "x" ]]; then
-#    messages=0
-    echo "<img>/home/marko/.scripts/genmon/skype-icon-inactive.png</img>"
+# if [[ $(wmctrl -l | grep Skype™) ]]; then echo "Ellow"; else echo "Noooo"; fi
+
+if [[ $skype ]]; then
+    
+    messages=$(echo $skype | awk '{ print $4 }' | grep -i '\[[0-9]*\]' | cut -d ] -f 1 | cut -d [ -f 2)
+    
+    if [[ "x$messages" == "x" ]]; then
+        #    messages=0
+        echo "<img>/home/marko/.scripts/genmon/skype-icon-inactive.png</img>"
+    else
+        echo "<img>/home/marko/.scripts/genmon/skype-icon.png</img>"
+    fi
+
+    echo "<click>/home/marko/.scripts/scripts/to.sh Skype™</click>"
+    # echo "<txt> "$messages"</txt>"
 else
-    echo "<img>/home/marko/.scripts/genmon/skype-icon.png</img>"
+    echo "<img>/home/marko/.scripts/genmon/skype-icon-missing.png</img>"
 fi
-
-echo "<click>/home/marko/.scripts/scripts/to.sh Skype™</click>"
-# echo "<txt> "$messages"</txt>"
